585f5ff9e7c59257368ec37d0dbc7defbbd00c24
Jason Downing <jdown1994@gmail.com>
2017-01-22 23:21:27 -0500

    adding test php script.

---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Json
+ * Date: 1/22/17
+ * Time: 11:17 PM
+ */
+
+/**
+        From the following URL: https://secure.php.net/manual/en/tutorial.firstpage.php
+*/
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hello World</p>'; ?>
+</body>
+</html>
